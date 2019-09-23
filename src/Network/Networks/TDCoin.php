c5cae997d76ea390fba69a43af1fca084f143248
AVZ <tatdig@itservicefl.com>
2019-09-23 10:06:20 -0400

    TDCoin network files

---
+++ src/Network/Networks/TDCoin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace BitWasp\Bitcoin\Network\Networks;
+
+use BitWasp\Bitcoin\Network\Network;
+use BitWasp\Bitcoin\Script\ScriptType;
+
+class TDCoin extends Network
+{
+    /**
+     * {@inheritdoc}
+     * @see Network::$base58PrefixMap
+     */
+    protected $base58PrefixMap = [
+        self::BASE58_ADDRESS_P2PKH => "41",
+        self::BASE58_ADDRESS_P2SH => "52",
+        self::BASE58_WIF => "6b",
+    ];
+
+    /**
+     * {@inheritdoc}
+     * @see Network::$bech32PrefixMap
+     */
+    protected $bech32PrefixMap = [
+        self::BECH32_PREFIX_SEGWIT => "tc",
+    ];
+
+    /**
+     * {@inheritdoc}
+     * @see Network::$bip32PrefixMap
+     */
+    protected $bip32PrefixMap = [
+        self::BIP32_PREFIX_XPUB => "0488b21e",
+        self::BIP32_PREFIX_XPRV => "0488ade4",
+    ];
+
+    /**
+     * {@inheritdoc}
+     * @see Network::$bip32ScriptTypeMap
+     */
+    protected $bip32ScriptTypeMap = [
+        self::BIP32_PREFIX_XPUB => ScriptType::P2PKH,
+        self::BIP32_PREFIX_XPRV => ScriptType::P2PKH,
+    ];
+
+    /**
+     * {@inheritdoc}
+     * @see Network::$signedMessagePrefix
+     */
+    protected $signedMessagePrefix = "TDCoin Signed Message";
+
+    /**
+     * {@inheritdoc}
+     * @see Network::$p2pMagic
+     */
+    protected $p2pMagic = "58f5a612";
+}
---
+++ src/Network/Networks/TDCoinRegtest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BitWasp\Bitcoin\Network\Networks;
+
+class BitcoinRegtest extends BitcoinTestnet
+{
+    protected $p2pMagic = "1e2bfc12";
+}
